ff8cea07cca62769d64cd14d64485b31a22fec21
Vanusa Santos <77839302+Baianissima@users.noreply.github.com>
2021-12-14 18:23:12 +0100

    suite PHP + suite requetes SQL

--- 02_variables/01_variables.php
+++ 02_variables/01_variables.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 
-<body class="bg-vieuxrose>
+<body class="bg-vieuxrose">
     <!-- =================================== -->
     <!-- en-tête -->
     <!-- =================================== -->
@@ -22,6 +22,9 @@
         <div class="col-12 text-center">
             <h1 class="display-4">Cours PHP - Chapitre 02 - Variables</h1>
             <p class="lead">Les variables en PHP</p>
+
+            <!-- ce code en 1 ligne sert à afficher le chemnin du fichier sur la page html -->
+            <?php echo "<p>Exemple de constante en PHP >>> Chemin absolu du fichier en cours : " . __FILE__ . "</p>"; ?>
         </div>
     </header>
 
@@ -29,8 +32,14 @@
 
         <div class="row">
             <div class="col-md-6">
-            <h2>Les variables</h2>
-            <p>Chaque variable possède un identifiant particulier, qui commence toujours par le caractère dollar ($) suivi de la variablr. Les règles de crétion des noms de la variable :</p>
+                <h2>Les variables</h2>
+                <p>Chaque variable possède un identifiant particulier, qui commence toujours par le caractère dollar ($) suivi de la variable. Les règles de créa tion des noms de la variable sont les suivantes :</p>
+
+                <ul>
+                    <li>Le nom commence par un caractère alphabétique, pris dans les ensembles [a-z], [A-Z] ou par le tiret du bas </li>
+                    <li>Les caractères suivants peuvent être les mêmes plus des chiffres.</li>
+                    <li>La longueur du nom n’est pas limitée, mais il convient d’être raisonnable sous peine de confusion dans la saisie du code. Il est conseillé de créer des noms de variable le plus « parlant » possible. En relisant le code contenant la variable $nomclient, par exemple, vous comprenez davantage ce que vous manipulez que si vous aviez écrit $x ou $y.</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -39,10 +48,11 @@
 
   <!-- Commenter à l'intérieur du code PHP si on veut garder l'info que pour le côté serveur !  -->
 
-  <!-- <?php require 
+  <?php require
     // Ici on appelle le footer cree dans dossier inc pour qu il se repete sur cette page : 
-  '../inc/footer.inc.php'; ?> -->
+  '../inc/footer.inc.php'; ?>
 
+<!-- ici on a l'includ pour synroniser le code du footer sur toutes les pages du dossier : -->
   <?php require_once '../inc/footer.inc.php'; ?>
 
     <!-- Optional JavaScript -->
@@ -50,11 +60,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
         crossorigin="anonymous"></script>
-
-    <!-- Démarrage PHP : repos coursPHP sur OS (C:) : il faut ouvrir le fichier index de localhost pour travailler sur php, sinon la page ne marche pas : 
-
-    http://localhost/coursPHP/01-intro/01_introduction.php  -->
-
-    <!-- https://github.com/vienendelsur/coursPHP_suresnes2122 -->
 </body>
 </html>
--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,6 +1,10 @@
 <footer>
     <!-- Ici on demande à PHP d'afficher l'heure au format français -->
     <div class="container bg-warning text-white">
+
+ <!-- ce code avec 1 constante en PHP en 1 ligne sert à afficher le chemin du fichier sur la page html -->
+    <?php echo "<p>Exemple de constante en PHP >>> Chemin absolu du fichier en cours : " . __FILE__ . "</p>"; ?>
+        
         <p>
         <?php 
             setlocale(LC_ALL, 'fr_FR');
